37c3242baa38f3eb80aa28173ec6ef2d2a92b22a
20213136 <chehrazad.ouazzani@student.vinci.be>
2023-10-16 11:12:47 +0200

    version Enum

--- src/Bear.java
+++ src/Bear.java
@@ -1,9 +1,47 @@
 //Patern State
 public class Bear {
-    private State state;
-    public Bear() {
-        setState(new NotSleepingState());
+
+    public enum State {
+        SLEEPING {
+            @Override
+            public void attack(Bear b) {
+                System.out.println("Zzzzzz");
+            }
+
+            @Override
+            public void sleep(Bear b) {
+                throw new RuntimeException("already sleeping");
+            }
+        },
+        NOTSLEEPING {
+            @Override
+            public void attack(Bear b) {
+                System.out.println("GroaaR");
+            }
+
+            @Override
+            public void wakeup(Bear bear) {
+                throw new RuntimeException("already not sleeping");
+            }
+        };
+
+        public abstract void attack(Bear b);
+
+        public void sleep(Bear b) {
+            b.setState(SLEEPING);
+        }
+
+        public void wakeup(Bear bear) {
+            bear.setState(NOTSLEEPING);
+        }
     }
+
+    private State state;
+
+    public Bear() {
+        setState(State.NOTSLEEPING);
+    }
+
     public void attack() {
         state.attack(this);
     }
@@ -17,11 +55,11 @@ public class Bear {
     }
 
     public void setState(State state) {
-        this.state= state;
+        this.state = state;
     }
 
     public static void main(String[] args) {
-        Bear b= new Bear();
+        Bear b = new Bear();
         b.attack();
         b.sleep();
         b.attack();
